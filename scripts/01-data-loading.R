6fdf9152eb15e1df14a5eb046916a927d3b10c3d
Yuxuan Yang <yuxuanmaggie.yang@mail.utoronto.ca>
2022-02-21 15:49:36 +0000

    Loaded data required to replicate Figure 2e in the original paper

--- scripts/01-data-loading.R
+++ scripts/01-data-loading.R
@@ -14,3 +14,12 @@ fig_2ab_data <- read_csv("inputs/data/figs_2a_2b.csv")
 write_csv(fig_2ab_data, "inputs/data/fig_2ab_data.csv")
 
 read_csv("inputs/data/fig_2ab_data.csv")
+
+# Load Figure 2e data into R Studio
+fig_2e_data <- read_csv("inputs/data/fig_2e.csv")
+
+write_csv(fig_2e_data, "inputs/data/fig_2e_data.csv")
+
+read_csv("inputs/data/fig_2e_data.csv")
+
+
